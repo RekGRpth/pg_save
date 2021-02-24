343495b8456425762718644013c72f52252c7725
RekGRpth <rekgrpth@gmail.com>
2021-02-24 12:33:07 +0500

    user data

--- init.c
+++ init.c
@@ -2,6 +2,10 @@
 
 PG_MODULE_MAGIC;
 
+static char *data;
+static char *schema;
+static char *table;
+static char *user;
 static int timeout;
 volatile sig_atomic_t sighup = false;
 volatile sig_atomic_t sigterm = false;
@@ -40,7 +44,7 @@ static void save_work(void) {
     if (buf.len + 1 > BGW_MAXLEN) E("%i > BGW_MAXLEN", buf.len + 1);
     memcpy(worker.bgw_type, buf.data, buf.len);
     resetStringInfo(&buf);
-    appendStringInfoString(&buf, "postgres postgres pg_save");
+    appendStringInfo(&buf, "%s %s %s", user, data, worker.bgw_type);
     if (buf.len + 1 > BGW_MAXLEN) E("%i > BGW_MAXLEN", buf.len + 1);
     memcpy(worker.bgw_name, buf.data, buf.len);
     pfree(buf.data);
@@ -50,6 +54,10 @@ static void save_work(void) {
 void _PG_init(void); void _PG_init(void) {
     if (IsBinaryUpgrade) return;
     if (!process_shared_preload_libraries_in_progress) F("!process_shared_preload_libraries_in_progress");
-    DefineCustomIntVariable("pg_save.timeout", "pg_save timeout", NULL, &timeout, 5000, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    DefineCustomIntVariable("pg_save.timeout", "pg_save timeout", NULL, &timeout, 1000, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    DefineCustomStringVariable("pg_save.data", "pg_save table", NULL, &data, "postgres", PGC_POSTMASTER, 0, NULL, NULL, NULL);
+    DefineCustomStringVariable("pg_save.schema", "pg_save schema", NULL, &schema, NULL, PGC_POSTMASTER, 0, NULL, NULL, NULL);
+    DefineCustomStringVariable("pg_save.table", "pg_save table", NULL, &table, "save", PGC_POSTMASTER, 0, NULL, NULL, NULL);
+    DefineCustomStringVariable("pg_save.user", "pg_save table", NULL, &user, "postgres", PGC_POSTMASTER, 0, NULL, NULL, NULL);
     save_work();
 }
--- save.c
+++ save.c
@@ -1,5 +1,9 @@
 #include "include.h"
 
+extern char *data;
+extern char *schema;
+extern char *table;
+extern char *user;
 extern int timeout;
 extern volatile sig_atomic_t sighup;
 extern volatile sig_atomic_t sigterm;
@@ -10,23 +14,35 @@ static void save_timeout(void) {
 static void save_socket(void *data) {
 }
 
+static void save_schema(void) {
+}
+
+static void save_type(void) {
+}
+
+static void save_table(void) {
+    if (schema) save_schema();
+    save_type();
+}
+
 #define SyncStandbysDefined() (SyncRepStandbyNames != NULL && SyncRepStandbyNames[0] != '\0')
 static void save_check(void) {
     if (SyncStandbysDefined()) {
     }
+    if (!StandbyMode) save_table();
 }
 
 static void save_init(void) {
     if (!EnableHotStandby) E("!EnableHotStandby");
     if (!MyProcPort && !(MyProcPort = (Port *)calloc(1, sizeof(Port)))) E("!calloc");
-    if (!MyProcPort->user_name) MyProcPort->user_name = "postgres";
-    if (!MyProcPort->database_name) MyProcPort->database_name = "postgres";
+    if (!MyProcPort->user_name) MyProcPort->user_name = user;
+    if (!MyProcPort->database_name) MyProcPort->database_name = data;
     if (!MyProcPort->remote_host) MyProcPort->remote_host = "[local]";
     set_config_option("application_name", MyBgworkerEntry->bgw_type, PGC_USERSET, PGC_S_SESSION, GUC_ACTION_SET, true, ERROR, false);
     pqsignal(SIGHUP, init_sighup);
     pqsignal(SIGTERM, init_sigterm);
     BackgroundWorkerUnblockSignals();
-    BackgroundWorkerInitializeConnection("postgres", "postgres", 0);
+    BackgroundWorkerInitializeConnection(data, user, 0);
     pgstat_report_appname(MyBgworkerEntry->bgw_type);
     process_session_preload_libraries();
 }
