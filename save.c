c7286161d9dd3b8cdf80b8d7a6b5d15cbb89c424
RekGRpth <rekgrpth@gmail.com>
2021-02-26 16:15:09 +0500

    curl

--- save.c
+++ save.c
@@ -2,9 +2,19 @@
 #include "include.h"
 #include <unistd.h>
 
+typedef struct FileString {
+    char *data;
+    FILE *file;
+    size_t len;
+} FileString;
+
 extern int timeout;
+static char errbuf[CURL_ERROR_SIZE];
 static char *hostname;
 static CURL *curl = NULL;
+static FileString header_str = {NULL, NULL, 0};
+static FileString write_str = {NULL, NULL, 0};
+static struct curl_slist *header = NULL;
 volatile sig_atomic_t sighup = false;
 volatile sig_atomic_t sigterm = false;
 
@@ -53,6 +63,24 @@ static text *pg_save_convert_from(bytea *string, const char *encoding_name) {
     return pg_save_convert(string, src_encoding_name, dest_encoding_name);
 }
 
+static void pg_save_easy_reset(void) {
+    curl_easy_reset(curl);
+    curl_slist_free_all(header);
+    header = NULL;
+    if (header_str.data) { free(header_str.data); header_str.data = NULL; }
+    if (write_str.data) { free(write_str.data); write_str.data = NULL; }
+}
+
+static struct curl_slist *pg_save_header_append(const char *name, const char *value) {
+    StringInfoData buf;
+    struct curl_slist *temp = header;
+    initStringInfo(&buf);
+    appendStringInfo(&buf, "%s: %s", name, value);
+    if ((temp = curl_slist_append(temp, buf.data))) header = temp; else E("!curl_slist_append");
+    pfree(buf.data);
+    return temp;
+}
+
 static CURLcode pg_save_easy_setopt_char(CURLoption option, const char *parameter) {
     CURLcode res = CURL_LAST;
     if ((res = curl_easy_setopt(curl, option, parameter)) != CURLE_OK) E("curl_easy_setopt(%i, %s): %s", option, parameter, curl_easy_strerror(res));
@@ -61,16 +89,71 @@ static CURLcode pg_save_easy_setopt_char(CURLoption option, const char *paramete
 
 static CURLcode pg_save_easy_setopt_copypostfields(bytea *parameter) {
     CURLcode res = CURL_LAST;
+    pg_save_easy_reset();
     if ((res = curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, VARSIZE_ANY_EXHDR(parameter))) != CURLE_OK) E("curl_easy_setopt(CURLOPT_POSTFIELDSIZE): %s", curl_easy_strerror(res));
     if ((res = curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, VARDATA_ANY(parameter))) != CURLE_OK) E("curl_easy_setopt(CURLOPT_COPYPOSTFIELDS): %s", curl_easy_strerror(res));
     PG_RETURN_BOOL(res == CURLE_OK);
     return res;
 }
 
+static CURLcode pg_save_easy_perform(int try, long sleep) {
+    CURLcode res = CURL_LAST;
+    if (try <= 0) E("try <= 0!");
+    if (sleep < 0) E("sleep < 0!");
+    while (try--) {
+        errbuf[0] = 0;
+        if (header_str.data) { free(header_str.data); header_str.data = NULL; }
+        if (write_str.data) { free(write_str.data); write_str.data = NULL; }
+        if (!(header_str.file = open_memstream(&header_str.data, &header_str.len))) E("!open_memstream");
+        if (!(write_str.file = open_memstream(&write_str.data, &write_str.len))) E("!open_memstream");
+        if ((res = curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf)) != CURLE_OK) E("curl_easy_setopt(CURLOPT_ERRORBUFFER): %s", curl_easy_strerror(res));
+        if ((res = curl_easy_setopt(curl, CURLOPT_HEADERDATA, header_str.file)) != CURLE_OK) E("curl_easy_setopt(CURLOPT_HEADERDATA): %s", curl_easy_strerror(res));
+        if ((res = curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L)) != CURLE_OK) E("curl_easy_setopt(CURLOPT_NOPROGRESS): %s", curl_easy_strerror(res));
+        if ((res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, write_str.file)) != CURLE_OK) E("curl_easy_setopt(CURLOPT_WRITEDATA): %s", curl_easy_strerror(res));
+        if (header && ((res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header)) != CURLE_OK)) E("curl_easy_setopt(CURLOPT_HTTPHEADER): %s", curl_easy_strerror(res));
+        switch (res = curl_easy_perform(curl)) {
+            case CURLE_OK: try = 0; break;
+            case CURLE_UNSUPPORTED_PROTOCOL:
+            case CURLE_FAILED_INIT:
+            case CURLE_URL_MALFORMAT:
+            case CURLE_NOT_BUILT_IN:
+            case CURLE_FUNCTION_NOT_FOUND:
+            case CURLE_BAD_FUNCTION_ARGUMENT:
+            case CURLE_UNKNOWN_OPTION:
+            case CURLE_LDAP_INVALID_URL:
+            case CURLE_ABORTED_BY_CALLBACK: try = 0; // fall through
+            default: {
+                if (try) {
+                    if (strlen(errbuf)) W("curl_easy_perform: %s: %s", curl_easy_strerror(res), errbuf);
+                    else W("curl_easy_perform: %s", curl_easy_strerror(res));
+                    if (sleep) pg_usleep(sleep);
+                } else {
+                    if (strlen(errbuf)) E("curl_easy_perform: %s: %s", curl_easy_strerror(res), errbuf);
+                    else E("curl_easy_perform: %s", curl_easy_strerror(res));
+                }
+            }
+        }
+        fclose(header_str.file);
+        fclose(write_str.file);
+    }
+    return res;
+}
+
+static text *pg_save_easy_getinfo_headers(void) {
+    if (!header_str.data) return NULL;
+    return cstring_to_text_with_len(header_str.data, header_str.len);
+}
+
+static bytea *pg_save_easy_getinfo_response(void) {
+    if (!write_str.data) return NULL;
+    return cstring_to_text_with_len(write_str.data, write_str.len);
+}
+
 static void save_set(const char *state) {
-    text *string = cstring_to_text(state);
+    text *string = cstring_to_text("{\"key\": \"Zm9v\", \"value\": \"YmFy\"}");
     if (pg_save_easy_setopt_char(CURLOPT_URL, "http://localhost:2379/v3/kv/put") != CURLE_OK) return;
     if (pg_save_easy_setopt_copypostfields(pg_save_convert_to(string, "utf-8")) != CURLE_OK) return;
+    if (pg_save_easy_perform(1, 1000000) != CURLE_OK) return;
 }
 
 static void save_timeout(void) {
@@ -126,7 +209,11 @@ static void save_init(void) {
 }
 
 static void save_fini(void) {
+    curl_easy_cleanup(curl);
+    curl_slist_free_all(header);
     curl_global_cleanup();
+    if (header_str.data) { free(header_str.data); header_str.data = NULL; }
+    if (write_str.data) { free(write_str.data); write_str.data = NULL; }
 }
 
 static void save_reload(void) {
