55ee7a32e05520752708795ce6b0259cfaf86ce6
RekGRpth <rekgrpth@gmail.com>
2021-03-03 12:56:29 +0500

    up

--- init.c
+++ init.c
@@ -3,13 +3,14 @@
 PG_MODULE_MAGIC;
 
 int timeout;
+static int restart;
 
 static void save_work(void) {
     StringInfoData buf;
     BackgroundWorker worker;
     MemSet(&worker, 0, sizeof(worker));
     worker.bgw_flags = BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION;
-    worker.bgw_restart_time = BGW_DEFAULT_RESTART_INTERVAL;
+    worker.bgw_restart_time = restart;
     worker.bgw_start_time = BgWorkerStart_ConsistentState;
     initStringInfo(&buf);
     appendStringInfoString(&buf, "pg_save");
@@ -35,5 +36,6 @@ void _PG_init(void); void _PG_init(void) {
     if (IsBinaryUpgrade) { W("IsBinaryUpgrade"); return; }
     if (!process_shared_preload_libraries_in_progress) F("!process_shared_preload_libraries_in_progress");
     DefineCustomIntVariable("pg_save.timeout", "pg_save timeout", NULL, &timeout, 1000, 1, INT_MAX, PGC_SIGHUP, 0, NULL, NULL, NULL);
+    DefineCustomIntVariable("pg_save.restart", "pg_save restart", NULL, &restart, BGW_DEFAULT_RESTART_INTERVAL, 1, INT_MAX, PGC_POSTMASTER, 0, NULL, NULL, NULL);
     save_work();
 }
