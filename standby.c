0c43d8b828088816b644b8541d5f08607bbe38f1
RekGRpth <rekgrpth@gmail.com>
2021-03-04 14:08:01 +0500

    test

--- standby.c
+++ standby.c
@@ -135,7 +135,8 @@ void standby_init(void) {
 }
 
 static void standby_standby(void) {
-    queue_each(&primary.queue, queue) { // for (queue_t *(q) = (h)->next; (q) != (h); (q) = (q)->next)
+    //queue_each(&primary.queue, queue) {
+    for (queue_t *(queue) = (&primary.queue)->next, *_; (queue) != (&primary.queue) && (_ = (queue)->next); (queue) = _) {
         Backend *standby = queue_data(queue, Backend, queue);
         PGresult *result;
         if (!(result = PQexec(standby->conn, "SELECT * FROM pg_stat_get_wal_receiver()"))) E("!PQexec and %s", PQerrorMessage(standby->conn));
