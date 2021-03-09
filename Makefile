817d6f447b8000517c7d9286dd2779015e7b1202
RekGRpth <rekgrpth@gmail.com>
2021-03-09 15:22:04 +0500

    reprimary

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 EXTENSION = pg_save
 MODULE_big = $(EXTENSION)
-OBJS = init.o save.o spi.o primary.o standby.o backend.o
+OBJS = init.o save.o spi.o primary.o standby.o backend.o recovery_gen.o string_utils.o
 PG_CONFIG = pg_config
 DATA = $(EXTENSION)--1.0.sql
-SHLIB_LINK = $(libpq)
+SHLIB_LINK = $(libpq) -lpgcommon_shlib
 PGXS := $(shell $(PG_CONFIG) --pgxs)
 include $(PGXS)
---
+++ string_utils.c
@@ -0,0 +1,50 @@
+#include "postgres_fe.h"
+
+#include <ctype.h>
+
+#include "common/keywords.h"
+#include "fe_utils/string_utils.h"
+
+/*
+ * Append the given string to the buffer, with suitable quoting for passing
+ * the string as a value in a keyword/value pair in a libpq connection string.
+ */
+void
+appendConnStrVal(PQExpBuffer buf, const char *str)
+{
+	const char *s;
+	bool		needquotes;
+
+	/*
+	 * If the string is one or more plain ASCII characters, no need to quote
+	 * it. This is quite conservative, but better safe than sorry.
+	 */
+	needquotes = true;
+	for (s = str; *s; s++)
+	{
+		if (!((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z') ||
+			  (*s >= '0' && *s <= '9') || *s == '_' || *s == '.'))
+		{
+			needquotes = true;
+			break;
+		}
+		needquotes = false;
+	}
+
+	if (needquotes)
+	{
+		appendPQExpBufferChar(buf, '\'');
+		while (*str)
+		{
+			/* ' and \ must be escaped by to \' and \\ */
+			if (*str == '\'' || *str == '\\')
+				appendPQExpBufferChar(buf, '\\');
+
+			appendPQExpBufferChar(buf, *str);
+			str++;
+		}
+		appendPQExpBufferChar(buf, '\'');
+	}
+	else
+		appendPQExpBufferStr(buf, str);
+}
