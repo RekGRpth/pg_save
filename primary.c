d110ee0b87c382b72de98ea78b1b51cfffc06a83
RekGRpth <rekgrpth@gmail.com>
2021-04-02 09:02:21 +0500

    -schema -extension

--- primary.c
+++ primary.c
@@ -44,52 +44,7 @@ void primary_finished(Backend *backend) {
 void primary_fini(void) {
 }
 
-static void primary_extension(const char *schema, const char *extension) {
-    StringInfoData buf;
-    List *names;
-    const char *schema_quote = schema ? quote_identifier(schema) : NULL;
-    const char *extension_quote = quote_identifier(extension);
-    D1("schema = %s, extension = %s", schema ? schema : "(null)", extension);
-    initStringInfoMy(TopMemoryContext, &buf);
-    appendStringInfo(&buf, "CREATE EXTENSION %s", extension_quote);
-    if (schema) appendStringInfo(&buf, " SCHEMA %s", schema_quote);
-    names = stringToQualifiedNameList(extension_quote);
-    SPI_connect_my(buf.data);
-    if (!OidIsValid(get_extension_oid(strVal(linitial(names)), true))) SPI_execute_with_args_my(buf.data, 0, NULL, NULL, NULL, SPI_OK_UTILITY, false);
-    else D1("extension %s already exists", extension_quote);
-    SPI_commit_my();
-    SPI_finish_my();
-    list_free_deep(names);
-    if (schema && schema_quote != schema) pfree((void *)schema_quote);
-    if (extension_quote != extension) pfree((void *)extension_quote);
-    pfree(buf.data);
-}
-
-static void primary_schema(const char *schema) {
-    StringInfoData buf;
-    List *names;
-    const char *schema_quote = quote_identifier(schema);
-    D1("schema = %s", schema);
-    initStringInfoMy(TopMemoryContext, &buf);
-    appendStringInfo(&buf, "CREATE SCHEMA %s", schema_quote);
-    names = stringToQualifiedNameList(schema_quote);
-    SPI_connect_my(buf.data);
-    if (!OidIsValid(get_namespace_oid(strVal(linitial(names)), true))) SPI_execute_with_args_my(buf.data, 0, NULL, NULL, NULL, SPI_OK_UTILITY, false);
-    else D1("schema %s already exists", schema_quote);
-    SPI_commit_my();
-    SPI_finish_my();
-    list_free_deep(names);
-    if (schema_quote != schema) pfree((void *)schema_quote);
-    pfree(buf.data);
-}
-
 void primary_init(void) {
-    primary_schema("async");
-    primary_extension("async", "pg_async");
-    primary_schema("curl");
-    primary_extension("curl", "pg_curl");
-    primary_schema("save");
-    primary_extension("save", "pg_save");
     init_set_system("primary_conninfo", NULL);
     if (init_state <= state_unknown) init_set_state(state_initial);
 }
