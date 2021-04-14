39aa353d45a77a7851c016e16af1dfb00d608b1a
RekGRpth <rekgrpth@gmail.com>
2021-04-14 13:53:19 +0500

    op

--- bin/main.c
+++ bin/main.c
@@ -1,12 +1,16 @@
 #include "bin.h"
 
 static void main_primary(void) {
+    PGconn *conn;
     switch (PQping(getenv("PRIMARY_CONNINFO"))) {
         case PQPING_NO_ATTEMPT: W("PQPING_NO_ATTEMPT"); return;
         case PQPING_NO_RESPONSE: W("PQPING_NO_RESPONSE"); return;
         case PQPING_OK: I("PQPING_OK"); break;
         case PQPING_REJECT: W("PQPING_REJECT"); return;
     }
+    if (!(conn = PQconnectdb(getenv("PRIMARY_CONNINFO")))) { W("!PQconnectdb and %.*s", (int)strlen(PQerrorMessage(conn)) - 1, PQerrorMessage(conn)); return; }
+    I("host = %s", PQhost(conn));
+    PQfinish(conn);
 }
 
 int main(int argc, char *argv[]) {
