36b90d44a70da4550bb58887d9849db67e623f08
RekGRpth <rekgrpth@gmail.com>
2021-04-14 13:34:30 +0500

    debug

--- bin/main.c
+++ bin/main.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char *argv[]) {
     pg_logging_init(argv[0]);
-    W("hi");
+    I("PRIMARY_CONNINFO = %s", getenv("PRIMARY_CONNINFO"));
+    //PRIMARY="$(chpst -u "$USER":"$GROUP" psql --quiet --tuples-only --no-align --dbname="$PRIMARY_CONNINFO" --command="SELECT current_setting('pg_save.hostname', false)" || echo)"
     return 0;
 }
