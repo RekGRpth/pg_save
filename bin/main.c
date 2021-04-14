63c5956da2175d8cf0a8987f7583c55011446f60
RekGRpth <rekgrpth@gmail.com>
2021-04-14 13:56:26 +0500

    op

--- bin/main.c
+++ bin/main.c
@@ -1,22 +1,27 @@
 #include "bin.h"
 
-static void main_primary(void) {
+static char *main_primary(void) {
+    char *host;
     PGconn *conn;
     switch (PQping(getenv("PRIMARY_CONNINFO"))) {
-        case PQPING_NO_ATTEMPT: W("PQPING_NO_ATTEMPT"); return;
-        case PQPING_NO_RESPONSE: W("PQPING_NO_RESPONSE"); return;
+        case PQPING_NO_ATTEMPT: W("PQPING_NO_ATTEMPT"); return NULL;
+        case PQPING_NO_RESPONSE: W("PQPING_NO_RESPONSE"); return NULL;
         case PQPING_OK: I("PQPING_OK"); break;
-        case PQPING_REJECT: W("PQPING_REJECT"); return;
+        case PQPING_REJECT: W("PQPING_REJECT"); return NULL;
     }
-    if (!(conn = PQconnectdb(getenv("PRIMARY_CONNINFO")))) { W("!PQconnectdb and %.*s", (int)strlen(PQerrorMessage(conn)) - 1, PQerrorMessage(conn)); return; }
-    I("host = %s", PQhost(conn));
+    if (!(conn = PQconnectdb(getenv("PRIMARY_CONNINFO")))) { W("!PQconnectdb and %.*s", (int)strlen(PQerrorMessage(conn)) - 1, PQerrorMessage(conn)); return NULL; }
+    host = strdup(PQhost(conn));
     PQfinish(conn);
+    return host;
 }
 
 int main(int argc, char *argv[]) {
+    char *primary;
     pg_logging_init(argv[0]);
     I("PRIMARY_CONNINFO = %s", getenv("PRIMARY_CONNINFO"));
     //PRIMARY="$(chpst -u "$USER":"$GROUP" psql --quiet --tuples-only --no-align --dbname="$PRIMARY_CONNINFO" --command="SELECT current_setting('pg_save.hostname', false)" || echo)"
-    main_primary();
+    primary = main_primary();
+    I("host = %s", primary ? primary : "(null)");
+    if (primary) free(primary);
     return 0;
 }
