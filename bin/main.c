c5b5838a0b08b1226c6c1a62fe835eaa92fbf0d4
RekGRpth <rekgrpth@gmail.com>
2021-04-14 16:20:52 +0500

    op

--- bin/main.c
+++ bin/main.c
@@ -16,28 +16,7 @@ static char *main_primary(void) {
 }
 
 static void main_check(void) {
-/*    char base[MAXPGPATH];
-    struct stat buf;
-    if (pg_mkdir_p(getenv("PGDATA"), pg_dir_create_mode) == -1) E("pg_mkdir_p(\"%s\") == -1 and %m", getenv("PGDATA"));
-    snprintf(base, sizeof(base), "%s/base", getenv("PGDATA"));
-    if (!stat(base, &buf) && S_ISDIR(buf.st_mode)) {
-    } else {
-    }
-    switch (pg_check_dir(getenv("PGDATA"))) {
-        case 0: I("directory \"%s\" does not exist", getenv("PGDATA")); break;
-        case 1: I("directory \"%s\" exists and empty", getenv("PGDATA")); break;
-        case 2: I("directory \"%s\" exists and contains _only_ dot files", getenv("PGDATA")); break;
-        case 3: I("directory \"%s\" exists and contains a mount point", getenv("PGDATA")); break;
-        case 4: I("directory \"%s\" exists and not empty", getenv("PGDATA")); break;
-        case -1: I("pg_check_dir(\"%s\") == -1 and %m", getenv("PGDATA")); break;
-    }*/
 }
-
-/*static char *main_dbname(const char *primary) {
-    static char dbname[MAXPGPATH];
-    snprintf(dbname, sizeof(dbname), "host=%s application_name=%s target_session_attrs=read-write", primary, getenv("HOSTNAME"));
-    return dbname;
-}*/
 
 static void main_recovery(void) {
     FILE *file;
@@ -135,11 +114,8 @@ static void main_init(void) {
 }
 
 int main(int argc, char *argv[]) {
-//    char *primary;
     pg_logging_init(argv[0]);
     I("PRIMARY_CONNINFO = %s", getenv("PRIMARY_CONNINFO"));
-//    if (pg_mkdir_p(getenv("ARCLOG"), pg_dir_create_mode) == -1) E("pg_mkdir_p(\"%s\") == -1 and %m", getenv("ARCLOG"));
-//    if (pg_mkdir_p(getenv("PGDATA"), pg_dir_create_mode) == -1) E("pg_mkdir_p(\"%s\") == -1 and %m", getenv("PGDATA"));
     switch (pg_check_dir(getenv("PGDATA"))) {
         case 0: E("directory \"%s\" does not exist", getenv("PGDATA")); break;
         case 1: I("directory \"%s\" exists and empty", getenv("PGDATA")); main_init(); break;
@@ -148,9 +124,5 @@ int main(int argc, char *argv[]) {
         case 4: I("directory \"%s\" exists and not empty", getenv("PGDATA")); main_check(); break;
         case -1: E("pg_check_dir(\"%s\") == -1 and %m", getenv("PGDATA")); break;
     }
-/*    //PRIMARY="$(chpst -u "$USER":"$GROUP" psql --quiet --tuples-only --no-align --dbname="$PRIMARY_CONNINFO" --command="SELECT current_setting('pg_save.hostname', false)" || echo)"
-    primary = main_primary();
-    I("host = %s", primary ? primary : "(null)");
-    if (primary) free(primary);
-    return 0;*/
+    return 0;
 }
