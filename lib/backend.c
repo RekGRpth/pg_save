96887e2ab5495595b47dd01ee0126726b2fbc912
RekGRpth <rekgrpth@gmail.com>
2021-05-06 13:11:08 +0500

    op

--- lib/backend.c
+++ lib/backend.c
@@ -215,17 +215,20 @@ void backend_fini(void) {
 }
 
 static void backend_idle_result(Backend *backend) {
-    for (PGresult *result; (result = PQgetResult(backend->conn)); PQclear(result)) {
+    PGresult *result;
+    while (PQstatus(backend->conn) == CONNECTION_OK) {
+        if (!PQconsumeInput(backend->conn)) { W("%s:%s !PQconsumeInput and %s and %.*s", backend->host, init_state2char(backend->state), backend_status(backend), (int)strlen(PQerrorMessage(backend->conn)) - 1, PQerrorMessage(backend->conn)); return; }
+        switch (PQflush(backend->conn)) {
+            case 0: break;
+            case 1: D1("PQflush == 1"); backend->event = WL_SOCKET_MASK; return;
+            case -1: W("%s:%s PQflush == -1 and %s and %.*s", backend->host, init_state2char(backend->state), backend_status(backend), (int)strlen(PQerrorMessage(backend->conn)) - 1, PQerrorMessage(backend->conn)); return;
+        }
+        if (PQisBusy(backend->conn)) { W("%s:%s PQisBusy", backend->host, init_state2char(backend->state)); backend->event = WL_SOCKET_READABLE; return; }
+        if (!(result = PQgetResult(backend->conn))) break;
         switch (PQresultStatus(result)) {
             default: D1("%s:%s PQresultStatus = %s and %.*s", backend->host, init_state2char(backend->state), PQresStatus(PQresultStatus(result)), (int)strlen(PQresultErrorMessage(result)) - 1, PQresultErrorMessage(result)); break;
         }
-        if (!PQconsumeInput(backend->conn)) { W("%s:%s !PQconsumeInput and %s and %.*s", backend->host, init_state2char(backend->state), backend_status(backend), (int)strlen(PQerrorMessage(backend->conn)) - 1, PQerrorMessage(backend->conn)); PQclear(result); return; }
-        switch (PQflush(backend->conn)) {
-            case 0: break;
-            case 1: D1("PQflush == 1"); backend->event = WL_SOCKET_MASK; PQclear(result); return;
-            case -1: W("%s:%s PQflush == -1 and %s and %.*s", backend->host, init_state2char(backend->state), backend_status(backend), (int)strlen(PQerrorMessage(backend->conn)) - 1, PQerrorMessage(backend->conn)); PQclear(result); return;
-        }
-        if (PQisBusy(backend->conn)) { W("%s:%s PQisBusy", backend->host, init_state2char(backend->state)); backend->event = WL_SOCKET_READABLE; PQclear(result); return; }
+        PQclear(result);
     }
 }
 
